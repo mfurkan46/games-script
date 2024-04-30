fe8173d9a0a1d294328f32d0b543d70170d98ca1
mfurkan46 <58640766+mfurkan46@users.noreply.github.com>
2024-04-30 17:23:59 +0300

    Update MarketSell.js

--- MarketSell.js
+++ MarketSell.js
@@ -5,7 +5,7 @@
 // @version     0.0.1
 // @author      @mfurkan46
 // @description Market Sell Bot
-// @downloadURL https://raw.githubusercontent.com/mfurkan46/games-script/main/MarketSell.js
+// @downloadURL https://github.com/mfurkan46/games-script/raw/main/MarketSell.js
 // ==/UserScript==
 
 
